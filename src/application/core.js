956b108b259c89a0232fc925d4174fc4e9824a1e
Chad Eubanks <ceubanks@thecodeboutique.com>
2017-07-26 18:56:53 -0700

    Start of sample application

---
+++ src/application/core.js
@@ -0,0 +1,6 @@
+// ==========================================================================
+// Project: Alto - JavaScript Application Framework
+// Copyright: @2017 The Code Boutique, LLC
+// License:   Intellectual property of The Code Boutique. LLC
+// Author: Chad Eubanks
+// ==========================================================================
